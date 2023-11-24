a51da98b8bfa87503fbd357bf398542c35194249
Loan <131166084+loanper@users.noreply.github.com>
2023-11-24 18:57:20 +0100

    Changement de logique pour le code en créant un tableau de structure

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-# optimisation-d-une-ligne-d-assemblage-ing2-tg-2023-2024-3-1
-Bienvenue dans notre projet d'optimisation d'une ligne d'assemblage en C!
+# Optimisation-d-une-ligne-d-assemblage-ing2-tg-2023-2024-3-1
+### Bienvenue dans notre projet d'optimisation d'une ligne d'assemblage en C!
 
-Equipe composée de : Arthur VAILLANT, Maxence SIMONET et Loan PERRARD
+##### Equipe composée de : Arthur VAILLANT, Maxence SIMONET et Loan PERRARD
--- main.c
+++ main.c
@@ -4,7 +4,12 @@
 
 int main() {
     int ordre = lire_ordre();
-    printf("%d", ordre);
+    t_operation* tabOpe = (t_operation*) malloc(sizeof (t_operation) * ordre);
+    creer_operation(tabOpe, ordre);
+    for (int i = 0; i < ordre; ++i) {
+        printf("%d %0.2f\n", tabOpe[i].numero, tabOpe[i].temps);
+    }
+    free(tabOpe);
     return 0;
 }
 
@@ -21,5 +26,31 @@ int lire_ordre(){
             ordre++;
         }
     }
+    fclose(file);
     return ordre;
+}
+
+void creer_operation(t_operation* tabOpe, int ordre){
+    FILE *file = fopen("../operations.txt", "r");
+    if (!file){
+        printf("probleme lecture du fichier operations.txt");
+        exit(-1);
+    }
+
+    for (int i = 0; i < ordre; i++) {
+        fscanf(file,"%d %f",&tabOpe[i].numero, &tabOpe[i].temps);
+        tabOpe[i].degre = 0;
+        tabOpe[i].couleur = 0;
+    }
+
+    fclose(file);
+}
+
+
+void contrainte_exclusion(t_operation* tabOpe, int ordre){
+    FILE *file = fopen("../exclusions.txt", "r");
+    if (!file){
+        printf("probleme lecture du fichier operations.txt");
+        exit(-1);
+    }
 }
