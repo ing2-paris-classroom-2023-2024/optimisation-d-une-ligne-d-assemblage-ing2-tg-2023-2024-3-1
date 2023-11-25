970e7869c600486308b530c3829f636baa0c769e
Arthur Vaillant <118492334+Brinjarthur@users.noreply.github.com>
2023-11-25 15:13:59 +0100

    Update main.c
    Ajout de fonction

--- main.c
+++ main.c
@@ -52,3 +52,33 @@ void creer_operation(t_operation* tabOpe, int ordre){
 }
 
 
+void contrainte_exclusion(t_operation* tabOpe, int ordre){
+    FILE *file = fopen("../exclusions.txt", "r");
+    if (!file){
+        printf("probleme lecture du fichier exclusions.txt");
+        exit(-1);
+    }
+
+    //Récupération de la valeur du sommet la plus élevée pour créer la matrice adéquate
+    int ordreExclusion = 0, op1, op2;
+
+    while (fscanf(file, "%d %d", &op1, &op2) == 2) {
+        if (op1 > ordreExclusion) ordreExclusion = op1;
+        if (op2 > ordreExclusion) ordreExclusion = op2;
+    }
+    rewind(file); //on revient au début du fichier
+
+    //Initialisation de la matrice dynamique
+    int** matriceExclusion = (int **) malloc(sizeof (int*) * ordreExclusion);
+    for (int i = 0; i < ordreExclusion; ++i) {
+        matriceExclusion[i] = (int*) malloc(sizeof (int) * ordreExclusion);
+    }
+
+    for (int i = 0; i < ordreExclusion; i++) {
+        for (int j = 0; j < ordreExclusion; j++) {
+            matriceExclusion[i][j] = 0;
+        }
+    }
+
+
+
